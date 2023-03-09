8525f69eaaf3bc1643cad2263b3a542c8660afb9
mmeah <mmeah2@gmail.com>
2023-03-08 22:46:24 -0600

    debug nose

--- script.js
+++ script.js
@@ -39,6 +39,8 @@ video.addEventListener('play', () => {
     ctx.fill(circle);
 
     const text = [
+      'NoseX:'+noseX,
+      'NoseY'+noseY,
       'This is a textline!',
       'This is another textline!'
     ]
