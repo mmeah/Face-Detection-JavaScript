1a8f135f37f2e69b117160b354e8c11eb98ba77d
mmeah <mmeah2@gmail.com>
2023-03-08 22:40:05 -0600

    circle on nose

--- script.js
+++ script.js
@@ -30,19 +30,14 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
     const ctx = canvas.getContext("2d");
-
-    ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-    ctx.moveTo(110, 75);
-    ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-    ctx.moveTo(65, 65);
-    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
-    ctx.moveTo(95, 65);
-    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
-    ctx.stroke();
+    const nose = landmarks.getNose();
+    const noseX = nose[0]._x;
+    const noseY = nose[0]._y;
+    const circle = new Path2D();
+    circle.arc(100+noseX, 35+noseY, 25, 0, 2 * Math.PI);
+    ctx.fill(circle);
 
     const text = [
-      JSON.stringify(landmarks.getNose()),
       'This is a textline!',
       'This is another textline!'
     ]
